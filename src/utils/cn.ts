beb1c7faa7b47bd39c5c564e6fa6e2d9d2b8944e
park-moen <pmj6516@naver.com>
2026-02-05 19:12:39 +0900

    fix: tailwind-merge 커스텀 directive 임시 처리
    tailwind-merge에서 typography custom directive가 적용되지 않는 문제를 임시로 해결했습니다.
    
    변경 내용:
    - extendTailwindMerge 설정
    - classGroups에 typo directive 추가 (font-size, font-weight, leading, tracking)
    - TODO: #103 Issue에서 근본적인 해결 후 제거 예정

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,7 +1,19 @@
 import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+// !todo: #103 Issue에서 현재 font-weight가 적용되지 않는 문제 해결 후 병합 예정
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [{ typo: [() => true] }],
+      'font-weight': [{ typo: [() => true] }],
+      'leading': [{ typo: [() => true] }],
+      'tracking': [{ typo: [() => true] }],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
