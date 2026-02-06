56bcd8f3f37e9be530535d6975a5dcdf264f4e7b
park-moen <pmj6516@naver.com>
2026-02-06 18:27:56 +0900

    refactor: barrel export 범위 최소화
    외부 페이지에서만 사용하는 컴포넌트만 barrel export로 공개했습니다.
    
    변경 내용:
    - 공개: Hero, PerformanceTabs (공연 목록 페이지에서 사용)
    - 제거: PastList, PerformanceCard, PerformanceSearch, UpcomingList (내부 컴포넌트)

--- src/app/performance-list/_components/index.ts
+++ src/app/performance-list/_components/index.ts
@@ -1,6 +1,2 @@
 export { Hero } from './hero';
-export { PastList } from './past-list';
-export { PerformanceCard } from './performance-card';
-export { PerformanceSearch } from './performance-search';
 export { PerformanceTabs } from './performance-tabs';
-export { UpcomingList } from './upcoming-list';
