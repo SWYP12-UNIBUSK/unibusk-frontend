8581f47729f25d424f85ab634032550befe0582d
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2026-02-06 09:37:55 +0900

    fix: 미사용 props 및 오타 수정

--- src/utils/busking-map/index.ts
+++ src/utils/busking-map/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export { adaptPerformanceLocationsToBuskingPlaces } from './performance-location.adapter';
-=======
-export { adaptPerformanceLocationsToBuskingPlaces } from './map-performance-locations-to-busking-places';
->>>>>>> 3b6b6bd (chore: performance-location 어댑터 추가)
