e7cad9959ac83d772f471b626bde826c3f1d63c7
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2026-02-14 10:52:16 +0900

    style: home-hero-section 레이아웃 수정

--- src/app/_components/home-container.tsx
+++ src/app/_components/home-container.tsx
@@ -7,5 +7,5 @@ interface HomeContainerProps {
 }
 
 export function HomeContainer({ children, className }: HomeContainerProps) {
-  return <div className={cn('mx-auto w-full max-w-360 px-6', className)}>{children}</div>;
+  return <section className={cn('mx-auto w-full max-w-360 px-6', className)}>{children}</section>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import { Header } from '@/components/common/header';
+import mapBg from '../../public/images/main-bg.webp';
+import heroIllust from '../../public/images/main-image.webp';
 import { HomeFooter, HomeHeroSection, HomePromoSection, HomeUpcomingBuskingSection } from './_components';
 
 export default function Page() {
   return (
     <main className="w-full">
-      <div
-        className="mx-auto my-5 flex h-16 w-full max-w-360 items-center px-6"
+      <section className={`
+        min-h-250 w-full
+        bg-[radial-gradient(circle_at_center,#FAFAFA_0%,#FFF7F2CC_100%)] pt-5
+      `}
       >
-        <Header />
-      </div>
-      <HomeHeroSection
-        mapBgSrc="/images/main-bg.webp"
-        heroIllustSrc="/images/main-image.webp"
-      />
+        <div className="mx-auto flex h-16 w-full max-w-360 items-center px-6">
+          <Header />
+        </div>
+
+        <HomeHeroSection mapBgSrc={mapBg} heroIllustSrc={heroIllust} />
+      </section>
 
       <HomeUpcomingBuskingSection
         title="다가오는 버스킹"
@@ -21,7 +25,6 @@ export default function Page() {
       />
 
       <HomePromoSection ctaHref="/performance-list" />
-
       <HomeFooter />
     </main>
   );
