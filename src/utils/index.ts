074f6cd840791281dd97cb1b8635e3e3e10dc054
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2026-03-23 10:30:38 +0900

    fix: 파일 오타 수정

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { cn } from './cn';
-export { createPageMetadata, SHARED_OPEN_GRAPH } from './creat-page-metadata';
+export { createPageMetadata, SHARED_OPEN_GRAPH } from './create-page-metadata';
 export { ENV } from './environment';
 export { isSameOrderedIds } from './id-equals';
 export { routePaths } from './route-paths';
