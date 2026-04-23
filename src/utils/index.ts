d8040f3d4166c9d3a70757f573b00f6fd4566c73
park-moen <pmj6516@naver.com>
2026-04-23 14:32:58 +0900

    feat: 전화번호 자동 포맷팅 유틸리티 함수 구현
    사용자 입력 시 전화번호를 실시간으로 포맷팅하는 유틸리티를 추가했습니다.
    
    구현 내용:
    - formatPhoneNumber: 숫자만 추출하고 패턴별 포맷팅
    - 02 지역번호: 02-XXXX-XXXX 또는 02-XXX-XXXX
    - 일반 번호: 010-XXXX-XXXX 또는 031-XXX-XXXX 등
    - 최대 11자리까지만 입력 가능
    - 입력 길이에 따라 동적으로 구분자("-") 자동 삽입
    - utils/index.ts에 export 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export { cn } from './cn';
 export { createPageMetadata, SHARED_OPEN_GRAPH } from './create-page-metadata';
 export { ENV } from './environment';
+export { formatPhoneNumber } from './format-phone-number';
 export { isSameOrderedIds } from './id-equals';
+
 export { routePaths } from './route-paths';
