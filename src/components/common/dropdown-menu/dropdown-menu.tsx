e728b10f0fde1ab7d763cea017c3e38be1d8ef5c
park-moen <pmj6516@naver.com>
2026-05-08 18:06:35 +0900

    fix: DropdownMenuSubContent z-index 토큰 누락 수정

--- src/components/common/dropdown-menu/dropdown-menu.tsx
+++ src/components/common/dropdown-menu/dropdown-menu.tsx
@@ -290,7 +290,8 @@ function DropdownMenuSubContent({
       data-slot="dropdown-menu-sub-content"
       className={cn(
         `
-          z-50 min-w-32 origin-(--radix-dropdown-menu-content-transform-origin)
+          z-dropdown min-w-32
+          origin-(--radix-dropdown-menu-content-transform-origin)
           overflow-hidden rounded-md border bg-popover p-1
           text-popover-foreground shadow-lg
           data-[side=bottom]:slide-in-from-top-2
