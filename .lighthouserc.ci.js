50c92bf7cf82b26395b6c9e9effdc727f16f169b
park-moen <pmj6516@naver.com>
2026-05-26 18:09:03 +0900

    perf: 테스트를 위해서 임시로 CI 테스트 횟수 3회에서 1회로 롤백

--- .lighthouserc.ci.js
+++ .lighthouserc.ci.js
@@ -10,7 +10,7 @@ module.exports = {
         `${baseUrl}/performance-list?tab=past`,
         `${baseUrl}/about-us`,
       ],
-      numberOfRuns: 3,
+      numberOfRuns: 1,
     },
     assert: {
       assertions: {
