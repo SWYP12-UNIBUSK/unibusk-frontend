d2a5541752f6278011f93a7086c42d9fe0ac8104
park-moen <pmj6516@naver.com>
2026-05-29 20:09:28 +0900

    feat: PerformanceCardSkeleton 컴포넌트 추가
    변경 내용:
    - PerformanceCard 레이아웃 기반 skeleton 컴포넌트 구현
    - performance-card 및 performance barrel export에 추가

--- src/components/performance/index.ts
+++ src/components/performance/index.ts
@@ -1,4 +1,4 @@
-export { PerformanceCard } from './performance-card';
+export { PerformanceCard, PerformanceCardSkeleton } from './performance-card';
 export { PerformanceDeleteConfirmDialog } from './performance-delete-confirm-dialog';
 export { PerformanceRegisterButton } from './performance-register-button';
 export { RegisterModal } from './register-modal';
